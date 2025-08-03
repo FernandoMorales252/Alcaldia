31d83b0076746c25e95d782d4241f0a2da120246
Elisandro Elias Flores Zaña <EliasEFloresZ2006@outlook.com>
2025-08-03 12:26:53 -0600

    get y set para Rol y Cargo

--- Alcaldia/src/main/java/com/SysGroup/Alcaldia/Modelos/Cargo.java
+++ Alcaldia/src/main/java/com/SysGroup/Alcaldia/Modelos/Cargo.java
@@ -1,5 +1,38 @@
 package com.SysGroup.Alcaldia.Modelos;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "Cargo")
 public class Cargo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id_cargo;
+    private String nombre_cargo;
+    private String descripcion;
+
+    public Integer getId_cargo() {
+        return id_cargo;
+    }
+
+    public void setId_cargo(Integer id_cargo) {
+        this.id_cargo = id_cargo;
+    }
+
+    public String getNombre_cargo() {
+        return nombre_cargo;
+    }
+
+    public void setNombre_cargo(String nombre_cargo) {
+        this.nombre_cargo = nombre_cargo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 
 }

03018d24778c873f7dcd484e3fa87417cc514d88
Nicole Ordoñez <NicoleMendoza1080@outlook.com>
2025-08-21 14:01:26 -0300

    Cambios de Municipio

---
+++ Alcaldia/src/main/java/com/SysGroup/Alcaldia/Servicios/Implementaciones/MunicipioService.java
@@ -0,0 +1,57 @@
+package com.SysGroup.Alcaldia.Servicios.Implementaciones;
+
+
+
+import java.util.*;
+
+import com.SysGroup.Alcaldia.Servicios.Interfaces.IMunicipioService;
+import com.SysGroup.Alcaldia.Modelos.Municipio;
+import com.SysGroup.Alcaldia.Repositorios.MunicipioRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.*;
+import org.springframework.data.domain.*;
+
+
+@Service
+public class MunicipioService implements IMunicipioService {
+    @Autowired
+    private MunicipioRepository municipioRepository;
+
+    @Override
+    public Page<Municipio> buscarTodos(Pageable pageable) {
+        return municipioRepository.findAll(pageable);
+    }
+
+    
+    @Override
+    public List<Municipio> obtenerTodosLosMunicipios() {
+        return municipioRepository.findAll();
+    }
+
+    @Override
+    public Optional<Municipio> buscaPorId(Integer id) {
+        return municipioRepository.findById(id);
+    }
+
+    @Override
+    public Municipio crearOEditar (Municipio municipio) {
+        return municipioRepository.save(municipio);
+    }
+
+    @Override
+    public void eliminarMunicipio(Integer id) {
+        municipioRepository.deleteById(id);
+    }
+    
+    
+    }
+
+   
+
+    
+
+    
+
+
+
---
+++ Alcaldia/src/main/java/com/SysGroup/Alcaldia/Servicios/Interfaces/IMunicipioService.java
@@ -0,0 +1,18 @@
+package com.SysGroup.Alcaldia.Servicios.Interfaces;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.SysGroup.Alcaldia.Modelos.Municipio;
+
+
+public interface IMunicipioService {
+    Page<Municipio> buscarTodos(Pageable pageable);
+    List<Municipio> obtenerTodosLosMunicipios();
+    void eliminarMunicipio(Integer id);
+    Municipio crearOEditar(Municipio municipio);
+    Optional<Municipio> buscaPorId(Integer id);
+}

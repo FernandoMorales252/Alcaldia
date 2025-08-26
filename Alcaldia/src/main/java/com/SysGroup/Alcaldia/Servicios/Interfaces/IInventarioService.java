1236f594d6f051f0d31fe5a53a6fca31eacb591a
Fátima Lisseth Jiménez Mendez <fatimalissjimenez@outlook.com>
2025-08-26 05:21:55 -0600

    cambio interfas inventario

---
+++ Alcaldia/src/main/java/com/SysGroup/Alcaldia/Servicios/Interfaces/IInventarioService.java
@@ -0,0 +1,28 @@
+package com.SysGroup.Alcaldia.Servicios.Interfaces;
+
+public interface IInventarioService {
+
+}
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.SysGroup.Alcaldia.Modelos.Inventario;
+
+public interface IInventarioService {
+
+
+     Page<Inventario> buscarTodos(Pageable pageable);
+
+    List<Inventario> obtenerTodos();
+
+    Optional<Inventario>  buscarPorId(Integer id_inventario);
+
+    Inventario crearOeditar(Inventario inventario);
+
+    void eliminarPorId(Integer id_inventario);
+
+}

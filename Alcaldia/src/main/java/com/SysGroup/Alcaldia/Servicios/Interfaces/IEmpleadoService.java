fe1875e520e6bce8044b26e73640dc588a979c16
LUIS FERNANDO MORALES PEREZ <Fernandomoe09@outlook.com>
2025-08-26 09:26:28 -0600

    Correcion de empleados en MainLayout

--- Alcaldia/src/main/java/com/SysGroup/Alcaldia/Servicios/Interfaces/IEmpleadoService.java
+++ Alcaldia/src/main/java/com/SysGroup/Alcaldia/Servicios/Interfaces/IEmpleadoService.java
@@ -1,7 +1,7 @@
 package com.SysGroup.Alcaldia.Servicios.Interfaces;
 
 import java.util.List;
-import java.util.Optional;
+
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
